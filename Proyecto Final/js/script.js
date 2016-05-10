1a0fda92d2f183d789cc80e61c2b9fbd61ed5a28
LuisCarlosBarahona <lcbarahonar@gmail.com>
2016-05-10 15:46:20 -0300

    Add files via upload
    Hasta ahí está el proyecto final con las funcionalidades de JavaScript, falta mejorar partes del diseño, pero hasta ahora es funcional.

---
+++ Proyecto Final/js/script.js
@@ -0,0 +1,420 @@
+var aMovie = [
+   {
+      "id": 1,
+      "title": "Funny People",
+      "year": "2009",
+      "released": "2009-04-29T00:00:00.0Z",
+      "genre": "Comedy, Drama",
+      "director": 1,
+      "actors": [
+         1,
+         6
+      ],
+      "plot": "When seasoned comedian George Simmons learns of his terminal, inoperable health condition, his desire to form a genuine friendship causes him to take a relatively green performer under his wing as his opening act.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNDQ5MTA5MF5BMl5BanBnXkFtZTcwMzUyMDUwMg@@._V1_SX300.jpg",
+	  "rating": 2.0
+   },
+   {
+      "id": 2,
+      "title": "The Hangover Part II",
+      "year": "2011",
+      "released": "2011-05-26T00:00:00.0Z",
+      "genre": "Comedy",
+      "director": 2,
+      "actors": [
+         2,
+         7
+      ],
+      "plot": "Right after the bachelor party in Las Vegas, Phil, Stu, Alan, and Doug jet to Thailand for Stu's wedding. Stu's plan for a subdued pre-wedding brunch, however, goes seriously awry.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTM2MTM4MzY2OV5BMl5BanBnXkFtZTcwNjQ3NzI4NA@@._V1_SX320.jpg",
+	  "rating": 3.5
+   },
+   {
+      "id": 3,
+      "title": "Fight Club",
+      "year": "1999",
+      "released": "1999-10-15T00:00:00.0Z",
+      "genre": "Drama",
+      "director": 3,
+      "actors": [
+         3,
+         8
+      ],
+      "plot": "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more...",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX300.jpg",
+	  "rating": 4.5
+   },
+   {
+      "id": 4,
+      "title": "Toy Story",
+      "year": "1995",
+      "released": "1995-01-22T00:00:00.0Z",
+      "genre": "Animation, Adventure, Comedy",
+      "director": 4,
+      "actors": [
+         4,
+         9
+      ],
+      "plot": "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTgwMjI4MzU5N15BMl5BanBnXkFtZTcwMTMyNTk3OA@@._V1_SX300.jpg",
+	  "rating": 4.2
+   },
+   {
+      "id": 5,
+      "title": "Forrest Gump",
+      "year": "1994",
+      "released": "1994-07-06T00:00:00.0Z",
+      "genre": "Drama, Romance",
+      "director": 5,
+      "actors": [
+         4,
+         10
+      ],
+      "plot": "Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTI1Nzk1MzQwMV5BMl5BanBnXkFtZTYwODkxOTA5._V1_SX300.jpg",
+	  "rating": 4.3
+   },
+ {
+      "id": 6,
+      "title":"Shrek",
+      "year": "2001",
+      "released": "2001-05-18T00:00:00.0Z",
+      "genre":"Animation, Adventure, Comedy",
+      "director": 6,
+      "actors": [
+         5,
+         11
+      ],
+      "plot": "After his swamp is filled with magical creatures, an ogre agrees to rescue a princess for a villainous lord in order to get his land back.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTk2NTE1NTE0M15BMl5BanBnXkFtZTgwNjY4NTYxMTE@._V1_SX300.jpg",
+	  "rating": 4.5
+   },
+	{
+      "id": 7,
+      "title":"Inglourious Basterds",
+      "year": "2009",
+      "released": "2009-08-21T00:00:00.0Z",
+      "genre":"Adventure, Drama, War",
+      "director": 7,
+      "actors": [
+         8,
+         12
+      ],
+      "plot": "In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner's vengeful plans for the same.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMjIzMDI4MTUzOV5BMl5BanBnXkFtZTcwNDY3NjA3Mg@@._V1_SX300.jpg",
+	  "rating": 3.9
+   },
+{
+      "id": 8,
+      "title":"Apoclaypse Now",
+      "year": "1979",
+      "released": "1979-08-15T00:00:00.0Z",
+      "genre":"Drama, War",
+      "director": 8,
+      "actors": [
+         13,
+         14
+      ],
+      "plot": "During the Vietnam War, Captain Willard is sent on a dangerous mission into Cambodia to assassinate a renegade colonel who has set himself up as a god among a local tribe.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTcyMzQ5NDM4OV5BMl5BanBnXkFtZTcwODUwNDg3OA@@._V1_SX300.jpg",
+	  "rating": 3.7
+   },
+{
+      "id": 9,
+      "title":"The Pianist",
+      "year": "2003",
+      "released": "2003-03-28T00:00:00.0Z",
+      "genre":"Biography, Drama, War",
+      "director": 9,
+      "actors": [
+         15,
+         16
+      ],
+      "plot": "A Polish Jewish musician struggles to survive the destruction of the Warsaw ghetto of World War II.",
+      "poster": "http://ia.media-imdb.com/images/M/MV5BMTc4OTkyOTA3OF5BMl5BanBnXkFtZTYwMDIxNjk5._V1_SX300.jpg",
+	  "rating": 3.5
+   },
+{
+      "id": 10,
+      "title":"A clockwork Orange",
+      "year": "1971",
+      "released": "1972-02-02T00:00:00.0Z",
+      "genre": "Crime, Drama, Sci-Fi",
+      "director": 10,
+      "actors": [
+         17,
+         18
+      ],
+      "plot": "In future Britain, charismatic delinquent Alex DeLarge is jailed and volunteers for an experimental aversion therapy developed by the government in an effort to solve society's crime problem",
+      "poster":"http://ia.media-imdb.com/images/M/MV5BMTY3MjM1Mzc4N15BMl5BanBnXkFtZTgwODM0NzAxMDE@._V1_SX300.jpg",
+	  "rating": 4.7
+   }
+];
+ var aDirectors= [
+   {
+      "id": 1,
+      "name": "Judd Apatow"
+   },
+   {
+      "id": 2,
+      "name": "Todd Phillips"
+   },
+   {
+      "id": 3,
+      "name": "David Fincher"
+   },
+   {
+      "id": 4,
+      "name": "John Lasseter"
+   },
+   {
+      "id": 5,
+      "name": "Robert Zemeckis"
+   },
+	{
+		"id": 6,
+        "name": "Andrew Adamson"
+	},
+	{
+		"id": 7,
+        "name": "Quentin Tarantino"
+	},
+	{
+		"id": 8,
+        "name": "Francis Ford Coppola"
+	},
+	{
+		"id": 9,
+        "name": "Roman Polanski"
+	},
+	{
+		"id": 10,
+        "name": "Staney Kubrick"
+	}
+];
+ var anActor = [{
+	"id": 1,
+	"name": "Adam Sandler"
+},{
+	"id": 2,
+	"name": "Bradley Cooper"
+},{
+	"id": 3,
+	"name": "Edward Norton"
+},{
+	"id": 4,
+	"name": "Tom Hanks"
+},{
+	"id": 5,
+	"name": "Mike Myers"
+},
+{
+	"id": 8,
+	"name": "Brad Pitt"
+},
+{
+	"id": 13,
+	"name": "Marlon Brando"
+},
+{
+	"id": 15,
+	"name": "Adrien Brody"
+},
+{
+	"id": 17,
+	"name": "Malcolm McDowell"
+}];
+var anotherActor = [{
+	"id": 6,
+	"name": "Seth Rogen"
+},{
+	"id": 7,
+	"name": "Zach Galifianakis"
+},{
+	"id": 8,
+	"name": "Brad Pitt"
+},{
+	"id": 9,
+	"name": "Tim Allen"
+},{
+	"id": 10,
+	"name": "Rebecca Williams"
+},{
+	"id": 11,
+	"name": "Eddie Murphy"
+},{
+	"id": 12,
+	"name": "Christoph Waltz"
+},{
+	"id": 14,
+	"name": "Martin Sheen"
+},{
+	"id": 16,
+	"name": "Emilia Fox"
+},{
+	"id": 18,
+	"name": "Patrick Magee"
+}];
+
+var data = [{
+	"movies": [aMovie],
+	"actors": [anActor,anotherActor],
+	"directors": [aDirectors]
+}];
+
+
+var getOrderedMoviesByReleased = function (){
+	
+		var currentYear = new Date();
+		var copyArray = data[0].movies[0].slice(0); //Create a clone of the original array to manipulate.
+
+
+		copyArray.sort(function(movie1,movie2){
+			var releasedDate1 = new Date(movie1.released);
+			var releasedDate2 = new Date(movie2.released);
+
+			if (releasedDate2<releasedDate1){
+				return -1;
+			} else if(releasedDate2>releasedDate1){
+				return 1;
+			}else{
+				return 0;
+			}
+		});
+
+		return copyArray;
+};
+
+//console.log(getOrderedMoviesByReleased());
+//console.log(data[0].movies[0]); It shows the original array, it can be compared in console with the ordered array 
+
+var getPopularMovies = function (){
+	
+		var copyArray = data[0].movies[0]; //Create a new reference of the original array to manipulate. (IT'S NOT A COPY!)
+
+		copyArray = copyArray.filter(function(movie){
+			/*if (movie.rating>=4.0){ This is what return movies does, 4 lines it can be reduced in just one below.
+				return true;
+			} else{
+				return false;
+			}*/
+		return movie.rating>=4.0;
+		});
+	return copyArray;	
+};
+			
+//console.log(getPopularMovies());
+
+var getAllMovies = function(){
+	return data[0].movies[0];
+};
+
+var getGenreMovies= function(){
+
+	var copyArray = data[0].movies[0];
+	var genresMovies= []; //An array with all genres
+	$.each(copyArray,function(index, movie){
+		var genresMovie = movie.genre.split(",");
+		for(var i=0; i<genresMovie.length;i++){
+			var genreMovie=genresMovie[i].trim(); // Iterate the genres of a movie. (Just one movie!) .trim is here because only here it becomes to string.
+			if (genresMovies.indexOf(genreMovie)===-1){ //Search if a single genre is on
+				genresMovies.push(genreMovie); // Put a genreMovie into the final array
+			}
+		}
+	});
+	return genresMovies;
+};
+//console.log(getGenreMovies());
+
+//var userValue= prompt("Type the key word");
+
+var getMatchUserData = function(userValue){
+
+	var copyArray = data[0].movies[0];
+	var match = [];
+
+	$.each(copyArray,function(index, movie){ //llego a las peliculas
+		var titleMovie = movie.title;
+		var directorMovieId = movie.director;
+		var directorMovie = null;
+		for (var i=0; i<data[0].directors[0].length;i++){ //llego a los directores
+			var director = data[0].directors[0][i]; //de cada iteración obtengo el director
+				if(directorMovieId===director.id){
+					directorMovie=director;
+				}
+
+		}
+
+		var actorIds=movie.actors;
+		var actors = [];
+		
+		for(var i=0; i<actorIds.length;i++){
+			var actorId = actorIds[i];
+			for (var j=0; j<data[0].actors.length;j++){ //llego a los actores
+				for (var k=0; k<data[0].actors[j].length; k++){
+					var actor = data[0].actors[j][k];
+					if(actor.id === actorId){
+						actors.push(actor);
+					}
+				}
+			}
+		}
+		if (titleMovie.toLowerCase().indexOf(userValue.toLowerCase())!==-1){
+			if(!verifyIfMovieExist(match, movie)){
+				match.push(movie);
+			}
+		}
+		if (directorMovie.name.toLowerCase().indexOf(userValue.toLowerCase())!==-1){
+			if(!verifyIfMovieExist(match, movie)){
+				match.push(movie);
+			}
+		} 
+		for(var i=0; i<actors.length; i++){
+			if(actors[i].name.toLowerCase().indexOf(userValue.toLowerCase())!==-1){
+				if(!verifyIfMovieExist(match, movie)){
+				match.push(movie);
+			}	
+			}
+		}
+
+	});
+	return match;
+};
+
+
+var verifyIfMovieExist = function(match, movie){
+var movieExist = false;
+	for(var i=0;i<match.length;i++){
+		if (match[i].id===movie.id){
+			movieExist = true;	
+			break;
+		}
+	}
+
+	return movieExist 
+};
+
+
+//console.log(getMatchUserData("Forrest"));
+//console.log(getMatchUserData("Brad"));
+
+var getMovieById = function(idMovie){
+	var copyArray = data[0].movies[0];
+	var movieById = [];
+	
+		for(var i=0; i<copyArray.length;i++){
+			if (copyArray[i].id===idMovie){
+				movieById.push(copyArray[i]);
+			}
+		}
+	return movieById;	
+}
+//console.log(getMovieById(5));
+
+
+
+
+
+
+
+//console.log(window.location);*/
